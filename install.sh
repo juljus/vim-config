6c5659c0ca6154e6f371d349b89e2be2979bc52b
juljus <juljus@gmail.com>
2022-09-23 22:07:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+rm .vimrc
+rmdir .vim
 curl -O https://raw.githubusercontent.com/juljus/vim-config/main/.vimrc
 mkdir -p .vim/autoload/
 cd .vim/autoload/
 curl -O https://raw.githubusercontent.com/juljus/vim-config/main/.vim/autoload/plug.vim
+cd ..
+cd ..
+rm install.sh
